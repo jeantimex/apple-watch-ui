a80755f27613e490c33f2fdd736cba4177d0f31f
Yong Su <jean.timex@gmail.com>
2015-04-02 14:38:36 -0700

    Fixed performance issue for setting app background image.

--- app/scripts/directives/app.js
+++ app/scripts/directives/app.js
@@ -12,14 +12,22 @@ angular
     restrict: 'E',
 
     link: function (scope, element, attrs) {
+      var inited = false;
+
       element.addClass('app');
 
       // Transform
       scope.$watch(attrs.app, function (app) {
+        if (!inited) {
+          element.css({
+            'background-image': 'url(images/apps/' + app.id + '.png)'
+          });
+          inited = true;
+        }
+
         var transformCSS = 'translate3d(' + app.x + 'px,' + app.y + 'px,' + app.z + 'px) scale(' + app.scale + ')';
         element.css({
-          'transform': transformCSS,
-          'background-image': 'url(images/apps/' + app.id + '.png)'
+          'transform': transformCSS
         });
       }, true);
     }
